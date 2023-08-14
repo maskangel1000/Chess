d2bf06623a3e744e2d2b2bd257c03af4c77e39dc
maskangel1000 <110355218+maskangel1000@users.noreply.github.com>
2023-08-14 16:38:55 -0500

    Update Game.py

--- src/scripts/Game.py
+++ src/scripts/Game.py
@@ -2,17 +2,14 @@ import pygame
 
 import Move
 
-WIDTH = 500
-HEIGHT = 500
-
 LIGHT = (227, 209, 200)
 DARK = (112, 92, 69)
 
-def draw_squares(screen, board):
+def draw_squares(screen, board, width, height):
     is_white = True
     for x in range(0, len(board.board)):
         for y in range(0, len(board.board[x])):
-            square = pygame.rect.Rect(x*WIDTH/8, y*HEIGHT/8, WIDTH/8, HEIGHT/8)
+            square = pygame.rect.Rect(x*width/8, y*height/8, width/8, height/8)
             if is_white:
                 pygame.draw.rect(screen, LIGHT, square)
             else:
@@ -20,46 +17,47 @@ def draw_squares(screen, board):
             is_white = not is_white
         is_white = not is_white
 
-def draw_pieces(screen, board, dragging, selected_piece):
+def draw_pieces(screen, board, dragging, selected_piece, width, height):
     drag_queue = None
     for row in range(0, len(board.board)):
         for col in range(0, len(board.board[row])):
             if board.board[row][col]:
                 piece_name = board.board[row][col].color + board.board[row][col].type
                 piece_image = pygame.image.load(f"../assets/{piece_name}.png")
-                piece_image = pygame.transform.scale(piece_image, (WIDTH//8, HEIGHT//8))
+                piece_image = pygame.transform.scale(piece_image, (width//8, height//8))
                 if dragging and selected_piece and selected_piece[1] == col and selected_piece[2] == row:
                     mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
                     mouse_x, mouse_y = mouse_pos
                     drag_queue = piece_image
                 else:
-                    screen.blit(piece_image, (col*WIDTH/8, row*HEIGHT/8))
+                    screen.blit(piece_image, (col*width/8, row*height/8))
     if drag_queue:
-        screen.blit(drag_queue, (mouse_x - (WIDTH//8) / 2, mouse_y - (HEIGHT//8) / 2))
+        screen.blit(drag_queue, (mouse_x - (width//8) / 2, mouse_y - (height//8) / 2))
     
-def get_mouse_square(board):
+def get_mouse_square(board, width, height):
+    print(width, height)
     mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
-    x = int(mouse_pos[0] // (WIDTH/8))
-    y = int(mouse_pos[1] // (HEIGHT/8))
+    x = int(mouse_pos[0] // (width/8))
+    y = int(mouse_pos[1] // (height/8))
     try:
         if x >= 0 and y >= 0:
             return board.board[y][x], x, y
     except IndexError:
         return None, None, None
     
-def draw_selector(board, screen, piece, x, y, dragging, selected_piece):
+def draw_selector(board, screen, piece, x, y, dragging, selected_piece, width, height):
     if not x or not y:
         return
-    rect = (x*WIDTH/8, y*HEIGHT/8, WIDTH/8, HEIGHT/8)
+    rect = (x*width/8, y*height/8, width/8, height/8)
     if dragging and selected_piece and selected_piece[0]:
         if Move.is_legal(board, selected_piece[1], selected_piece[2], x, y):
-            pygame.draw.rect(screen, (0, 255, 0, 50), rect, WIDTH//170)
+            pygame.draw.rect(screen, (0, 255, 0, 50), rect, width//130)
         else:
-            pygame.draw.rect(screen, (255, 0, 0, 50), rect, WIDTH//170)
+            pygame.draw.rect(screen, (255, 0, 0, 50), rect, width//130)
     elif piece and piece.color == board.turn:
-        pygame.draw.rect(screen, (0, 255, 0, 50), rect, WIDTH//170)
+        pygame.draw.rect(screen, (0, 255, 0, 50), rect, width//130)
 
-def end_game(screen, loser, board):
+def end_game(screen, loser, board, width, height):
     draw_squares(screen, board)
     draw_pieces(screen, board, None, None)
 
@@ -67,7 +65,7 @@ def end_game(screen, loser, board):
     same_color = (195, 195, 195) if winner == "White" else (60, 60, 60)
     opposite_color = (60, 60, 60) if winner == "White" else (195, 195, 195)
 
-    background = pygame.Surface((WIDTH, HEIGHT))
+    background = pygame.Surface((width, height))
     background.set_alpha(200)
     background.fill(same_color)
     screen.blit(background, (0, 0))
@@ -75,17 +73,87 @@ def end_game(screen, loser, board):
     button = pygame.Surface((200, 50))
     button.set_alpha(150)
     button.fill(opposite_color)
-    screen.blit(button, (WIDTH/2-100, HEIGHT/2))
+    screen.blit(button, (width/2-100, height/2))
 
     font = pygame.font.SysFont("Arial", 30)
 
     win_text = font.render(f"{winner} wins!", True, opposite_color)
-    screen.blit(win_text, (WIDTH/2-75, HEIGHT/2-50))
+    screen.blit(win_text, (width/2-75, height/2-50))
 
     play_text = font.render("PLAY AGAIN", True, same_color)
-    screen.blit(play_text, (WIDTH/2-85, HEIGHT/2+10))
+    screen.blit(play_text, (width/2-85, height/2+10))
+    
+def draw_menu(screen, width, height):
+    background = pygame.Surface((width, height))
+    background.set_alpha(200)
+    background.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    
+    play_button = pygame.Surface((200, 50))
+    play_button.set_alpha(150)
+    play_button.fill((255, 255, 255))
+    screen.blit(play_button, (width/2-100, height/2-height/10))
+    
+    settings_button = pygame.Surface((200, 50))
+    settings_button.set_alpha(150)
+    settings_button.fill((255, 255, 255))
+    screen.blit(settings_button, (width/2-100, height/2+height/10))
+    
+    font = pygame.font.SysFont("Arial", 40)
 
-def main(board):
+    play_text = font.render("PLAY", True, (0, 0, 0))
+    screen.blit(play_text, (width/2-85, height/2-height/10))
+    
+    settings_text = font.render("SETTINGS", True, (0, 0, 0))
+    screen.blit(settings_text, (width/2-85, height/2+height/10))
+    
+def draw_settings(screen, temp_width, temp_height, width, height):
+    background = pygame.Surface((width, height))
+    background.set_alpha(200)
+    background.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    
+    back_button = pygame.Surface((100, 50))
+    back_button.set_alpha(150)
+    back_button.fill((255, 255, 255))
+    screen.blit(back_button, (width/50, height/50))
+    
+    apply_button = pygame.Surface((110, 50))
+    apply_button.set_alpha(150)
+    apply_button.fill((255, 255, 255))
+    screen.blit(apply_button, (width - width/50 - 110, height/50))
+    
+    width_button = pygame.Surface((200, 50))
+    width_button.set_alpha(150)
+    width_button.fill((255, 255, 255))
+    screen.blit(width_button, (width/2-100, height/2+height/10))
+    
+    height_button = pygame.Surface((200, 50))
+    height_button.set_alpha(150)
+    height_button.fill((255, 255, 255))
+    screen.blit(height_button, (width/2-100, height/2-height/10))
+    
+    font = pygame.font.SysFont("Arial", 40)
+    
+    back_text = font.render("BACK", True, (0, 0, 0))
+    screen.blit(back_text, (width/35, height/50))
+    
+    apply_text = font.render("APPLY", True, (0, 0, 0))
+    screen.blit(apply_text, (width - width/35 - 100, height/50))
+
+    width_text = font.render("WIDTH", True, (255, 255, 255))
+    screen.blit(width_text, (width/2-85, height/2-height/5))
+    
+    height_text = font.render("HEIGHT", True, (255, 255, 255))
+    screen.blit(height_text, (width/2-85, height/2+height/5))
+
+    width_number = font.render(temp_width, True, (0, 0, 0))
+    screen.blit(width_number, (width/2-85, height/2-height/10))
+    
+    height_number = font.render(temp_height, True, (0, 0, 0))
+    screen.blit(height_number, (width/2-85, height/2+height/10))
+
+def main(board, width, height):
 
     # Init
 
@@ -93,7 +161,7 @@ def main(board):
     pygame.mixer.init()
     pygame.font.init()
 
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen = pygame.display.set_mode((width, height))
 
     pygame.display.set_caption("Chess")
 
@@ -111,6 +179,8 @@ def main(board):
     clock = pygame.time.Clock()
 
     running = True
+    menu = True
+    settings = False
     checkmate = False
     checkmate_button = False
 
@@ -125,17 +195,62 @@ def main(board):
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 dragging = True
+                mouse = pygame.mouse.get_pos()
                 if checkmate:
-                    mouse = pygame.mouse.get_pos()
-                    if WIDTH/2-100 <= mouse[0] <= WIDTH/2+100 and HEIGHT/2 <= mouse[1] <= HEIGHT/2+50:
+                    if width/2-100 <= mouse[0] <= width/2+100 and height/2 <= mouse[1] <= height/2+50:
                         checkmate_button = True
+                elif menu:
+                    if width/2-100 <= mouse[0] <= width/2+100 and height/2-height/10 <= mouse[1] <= height/2-height/10+50:
+                        menu = False
+                    if width/2-100 <= mouse[0] <= width/2+100 and height/2+height/10 <= mouse[1] <= height/2+height/10+50:
+                        menu = False
+                        settings = True
+                elif settings:
+                    if width/2-100 <= mouse[0] <= width/2+100 and height/2-height/10 <= mouse[1] <= height/2-height/10+50:
+                        typing_width = True
+                        typing_height = False
+                    elif width/2-100 <= mouse[0] <= width/2+100 and height/2+height/10 <= mouse[1] <= height/2+height/10+50:
+                        typing_height = True
+                        typing_width = False
+                    else:
+                        typing_width = False
+                        typing_height = False
+                        if width/50 <= mouse[0] <= width/50+100 and height/50 <= mouse[1] <= height/50+50:
+                            settings = False
+                            menu = True
+                        elif width - width/50 - 110 <= mouse[0] <= width - width/50 and height/50 <= mouse[1] <= height/50+50:
+                            try: temp_width, temp_height
+                            except NameError: temp_width, temp_height = "500", "500"
+                            
+                            width = int(temp_width)
+                            height = int(temp_height)
+                            
+                            main(board, width, height)
                 elif piece:
                     selected_piece = piece, x, y
+                    
+            if event.type == pygame.KEYDOWN:
+                if event.key != 8 and (event.key < 48 or event.key > 57):
+                    continue
+                
+                try: temp_width, temp_height
+                except NameError: temp_width, temp_height = "500", "500"
+                
+                if typing_width:
+                    if event.key == 8:
+                        temp_width = temp_width[:-1]
+                    else:
+                        temp_width += chr(event.key)
+                if typing_height:
+                    if event.key == 8:
+                        temp_height = temp_height[:-1]
+                    else:
+                        temp_height += chr(event.key)
             
             if event.type == pygame.MOUSEBUTTONUP:
                 if checkmate_button:
                     mouse = pygame.mouse.get_pos()
-                    if WIDTH/2-100 <= mouse[0] <= WIDTH/2+100 and HEIGHT/2 <= mouse[1] <= HEIGHT/2+50:
+                    if width/2-100 <= mouse[0] <= width/2+100 and height/2 <= mouse[1] <= height/2+50:
                         board.reset_board()
                         checkmate = False
                         checkmate_button = False
@@ -182,20 +297,29 @@ def main(board):
         pygame.display.set_icon(icon)
 
         # Updates
+
+        draw_squares(screen, board, width, height)
         
-
-        if not checkmate:
-
+        if not checkmate and not menu and not settings:
+            
             if not dragging: selected_piece = None
 
             try:
-                piece, x, y = get_mouse_square(board)
+                piece, x, y = get_mouse_square(board, width, height)
             except TypeError:
                 continue
 
-            draw_squares(screen, board)
-            draw_selector(board, screen, piece, x, y, dragging, selected_piece)
-            draw_pieces(screen, board, dragging, selected_piece)
+            draw_selector(board, screen, piece, x, y, dragging, selected_piece, width, height)            
+        
+        draw_pieces(screen, board, dragging, selected_piece, width, height)
+        
+        if menu:
+            draw_menu(screen, width, height)
+            
+        if settings:
+            try: temp_width, temp_height
+            except NameError: temp_width, temp_height = "500", "500"
+            draw_settings(screen, temp_width, temp_height, width, height)
 
         # Clock
 
